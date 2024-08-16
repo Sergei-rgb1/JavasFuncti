cfd80f6ee21855379c10763bb2f982f23770c345
Sergei-rgb1 <sergei25102007@bk.ru>
2024-08-16 17:21:01 +0400

    main

---
+++ script.js
@@ -0,0 +1,24 @@
+var avatar = "generic";
+var skill = 1.0;
+var pointsPerlevel = 1000;
+var userPoints = 2008;
+function getAvatar(points) {
+    var level = points / pointsPerlevel;
+    
+    if (level == 0) {
+        return "Teddy bear";
+        } else if (level == 1) {
+            return "Cat";
+        } else if (level >= 2) {
+            return "Gorilla";
+    }
+}
+
+function updatePoints (bonus, newPoints) {
+    var i = 0;
+    while (i < bonus) {
+        newPoints = newPoints + skill * bonus;
+        i = i +1;
+    }
+    return newPoints + userPoints;
+}
