848a5610e603b5766578856047b113191aa35998
Sergei-rgb@ <sergei25102007@bk.ru>
2024-08-20 17:11:06 +0400

    main

--- script.js
+++ script.js
@@ -24,3 +24,68 @@ function updatePoints (bonus, newPoints) {
 }
 userPoints = updatePoints(2, 100);
 avatar = getAvatar(2112);
+
+
+function dogYears(dogName, age) {
+    var years = age * 7;
+    console.log(dogName + " is " + years + "  years old");
+}
+var myDog = "Fido";
+dogYears(myDog, 4);
+
+function makeTea(cups, tea) {
+    console.log("Brewing" + cups + " cups of" + tea);
+}
+var guests = 3;
+makeTea(guests, "Earl Grey");
+
+function secret() {
+    console.log("The secret of line is 42");
+}
+secret();
+
+function speak (kind) {
+    var defaultSound = "";
+    if (kind == "dog") {
+        alert("Woof");
+    } else if (kind == "cat") {
+        alert("Meow");
+    } else {
+        alert(defaultSound);
+    }
+    
+}
+
+var pet = prompt("Enter a type of pet:");
+speak(pet);
+
+var x = 32;
+var y = 44;
+var radius = 5;
+
+var centerX = 0;
+var centerY = 0;
+var widht = 600;
+var height = 400;
+
+function setup(widht, height) {
+    centerX = widht/2
+    centerY = height/2 
+}
+
+function computeDistance (x1, y1, x2, y2,) {
+    var dx = x1 - x2;
+    var dy = y1 - y2;
+    var d2 = (dx * dx) + (dy * dy);
+    var d = Match.sqrt(d2);
+    return d;
+}
+
+function circleArea(r) {
+    var area = Math.PI * r * r;
+    return area;
+}
+
+setup(widht, height);
+var area = circleArea(radius);
+var distance = computeDistance(x, y, centerX, centerY);
